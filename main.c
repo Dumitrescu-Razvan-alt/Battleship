99e1ae635dc4de76193208dda7b7dfbb7df1a11c
Dumitrescu-Razvan-alt <razvan.dumitrescu04@gmail.com>
2025-12-02 13:00:03 +0200

    sparse changes

--- main.c
+++ main.c
@@ -7,13 +7,11 @@ typedef struct {
     bool active, head;
 } Triple;
 
-// Function to read input from user
 void readInput(int *x, char *orientation, int *row, int *col) {
     printf("Enter: x orientation row col\n");
     scanf("%d %c %d %d", x, orientation, row, col);
 }
 
-// Function to build sparse matrix
 Triple* buildSparseMatrix(int x, char orientation, int row, int col) {
     Triple *sparse = malloc(x * sizeof(Triple));
     if (!sparse) {
@@ -23,17 +21,15 @@ Triple* buildSparseMatrix(int x, char orientation, int row, int col) {
     bool head = 1;
     for (int i = 0; i < x; i++) {
         if (orientation == 'h') {
-            // horizontal: start from leftmost at (row, col)
             sparse[i].row = row;
             sparse[i].col = col + i;
             sparse[i].active = 1;
             sparse[i].head = head;
         } 
         else if (orientation == 'v') {
-            // vertical: start from bottom at (row, col)
             sparse[i].row = row - i;
             sparse[i].col = col;
-             sparse[i].active = 1;
+            sparse[i].active = 1;
             sparse[i].head = head;
         } 
         else {
@@ -42,13 +38,11 @@ Triple* buildSparseMatrix(int x, char orientation, int row, int col) {
             return NULL;
         }
         sparse[i].value = x;
-        head=0;
+        head = 0;
     }
-    
     return sparse;
 }
 
-// Function to print sparse matrix
 void printSparseMatrix(Triple *sparse, int x) {
     printf("\nSparse matrix (row col value):\n");
     for (int i = 0; i < x; i++) {
@@ -56,7 +50,6 @@ void printSparseMatrix(Triple *sparse, int x) {
     }
 }
 
-// Function to find the dimensions needed for dense matrix
 void findMatrixDimensions(Triple *sparse, int x, int *maxRow, int *maxCol) {
     *maxRow = sparse[0].row;
     *maxCol = sparse[0].col;
@@ -67,16 +60,12 @@ void findMatrixDimensions(Triple *sparse, int x, int *maxRow, int *maxCol) {
     }
 }
 
-// Function to print both sparse and dense representations
 void printBothRepresentations(Triple *sparse, int x) {
-    // Print sparse matrix
     printSparseMatrix(sparse, x);
     
-    // Find dimensions for dense matrix
     int maxRow, maxCol;
     findMatrixDimensions(sparse, x, &maxRow, &maxCol);
     
-    // Allocate and initialize dense matrix
     int **dense = malloc((maxRow + 1) * sizeof(int*));
     if (!dense) {
         printf("Memory allocation failed for dense matrix.\n");
@@ -87,7 +76,6 @@ void printBothRepresentations(Triple *sparse, int x) {
         dense[i] = calloc(maxCol + 1, sizeof(int));
         if (!dense[i]) {
             printf("Memory allocation failed for dense matrix row.\n");
-            // Free previously allocated rows
             for (int j = 0; j < i; j++) {
                 free(dense[j]);
             }
@@ -96,12 +84,10 @@ void printBothRepresentations(Triple *sparse, int x) {
         }
     }
     
-    // Fill dense matrix with values from sparse matrix
     for (int i = 0; i < x; i++) {
         dense[sparse[i].row][sparse[i].col] = sparse[i].value;
     }
     
-    // Print dense matrix
     printf("\nDense matrix representation:\n");
     for (int i = 0; i <= maxRow; i++) {
         for (int j = 0; j <= maxCol; j++) {
@@ -110,7 +96,6 @@ void printBothRepresentations(Triple *sparse, int x) {
         printf("\n");
     }
     
-    // Free dense matrix
     for (int i = 0; i <= maxRow; i++) {
         free(dense[i]);
     }
@@ -121,19 +106,16 @@ int main() {
     int x, row, col;
     char orientation;
     
-    // Read input
     readInput(&x, &orientation, &row, &col);
     
-    // Build sparse matrix
     Triple *sparse = buildSparseMatrix(x, orientation, row, col);
     if (!sparse) {
         return 1;
     }
     
-    // Print both representations
     printBothRepresentations(sparse, x);
     
-    // Clean up
     free(sparse);
     return 0;
 }
+
